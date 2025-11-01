7f8f94802db647f896a1c7d0e775874baf550eac
Thrivikram45 <btrivikram2006@gmail.com>
2025-11-01 21:12:03 +0530

    Update Image.jsx

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -6,7 +6,7 @@ const Image = () => {
     <div className="h-screen w-screen">
       <div
         className="
-          w-screen h-3/4 mt-2
+          h-3/4
           bg-cover bg-no-repeat bg-center
           bg-[url('https://images.unsplash.com/photo-1724579243950-3e49ae3560ee?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170')]"
       >
@@ -14,6 +14,9 @@ const Image = () => {
           <div className='pl-10 pt-5'> <Header /> </div>
       </div>
       <div className='h-1/4'>
+        <div className=" bg-blue-50 h-full pt-30 text-center text-sm text-gray-600">
+          © {new Date().getFullYear()} India Tourism. All rights reserved.
+        </div>
       </div>
     </div>
   );
